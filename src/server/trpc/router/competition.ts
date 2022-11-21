3cb7553519c3ba5606fcb02824e00c02c4da09e6
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-21 16:25:03 -0500

    Defined competition mutations and queries

---
+++ src/server/trpc/router/competition.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { router, protectedProcedure, publicProcedure } from "../trpc";
+
+export const competitionRouter = router({
+  createCompetition: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.competition.create({
+          data: {
+            name: input.name,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.competition.findMany({
+        select: {
+          name: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  }),
+});
