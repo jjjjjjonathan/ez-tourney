087626b169f06ce16be6267012551c5dd1a6a3e2
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-11-21 19:17:23 -0500

    Update list of competitions after creating new competition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import { trpc } from "../utils/trpc";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 const Competitions = () => {
   const { data: competitions, isLoading } = trpc.competition.getAll.useQuery();
@@ -20,6 +21,54 @@ const Competitions = () => {
   );
 };
 
+const Form = () => {
+  const [name, setName] = useState("");
+  const utils = trpc.useContext();
+  const postCompetition = trpc.competition.createCompetition.useMutation({
+    onMutate: () => {
+      onMutate: () => {
+        utils.competition.getAll.cancel();
+        const optimisticUpdate = utils.competition.getAll.getData();
+
+        if (optimisticUpdate) {
+          utils.competition.getAll.setData(optimisticUpdate);
+        }
+      };
+    },
+    onSettled: () => {
+      utils.competition.getAll.invalidate();
+    },
+  });
+
+  const { data: session } = useSession();
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (session !== null) {
+          postCompetition.mutate({
+            name,
+            userId: session.user?.id as string,
+          });
+        }
+        setName("");
+      }}
+    >
+      <input
+        type="text"
+        value={name}
+        placeholder="Your competition name..."
+        minLength={2}
+        maxLength={50}
+        onChange={(event) => setName(event.target.value)}
+        className="text-black"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
@@ -34,12 +83,15 @@ const Home: NextPage = () => {
         {session ? (
           <>
             <p>hi {session.user?.name}</p>
-            <Competitions />
+            <Form />
             <button onClick={() => signOut()}>logout</button>
           </>
         ) : (
           <button onClick={() => signIn("discord")}>login with discord</button>
         )}
+        <div>
+          <Competitions />
+        </div>
       </div>
     </main>
   );
